cc07bbc74b436b2ddedaa32e6a5e10f60f5c085d
Mitch Talmadge <mitcht@aptitekk.com>
2015-12-25 05:10:21 -0700

    More work on new GUI

---
+++ src/main/java/net/liveforcode/emojitools/gui2/tabcontrollers2/TabController.java
@@ -0,0 +1,119 @@
+/*
+ * Emoji Tools helps users and developers of Android, iOS, and OS X extract, modify, and repackage Emoji fonts.
+ * Copyright (C) 2015 Mitch Talmadge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact Mitch Talmadge at mitcht@liveforcode.net
+ */
+
+package net.liveforcode.emojitools.gui2.tabcontrollers2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import net.liveforcode.emojitools.EmojiTools;
+import net.liveforcode.emojitools.OperationManager;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public abstract class TabController implements Initializable {
+
+    @FXML
+    Label filePathTitleLabel;
+
+    @FXML
+    TextField filePathField;
+
+    @FXML
+    Button browseButton;
+
+    @FXML
+    Button openRootDirectoryButton;
+
+    @FXML
+    Button startButton;
+
+    OperationManager currentOperationManager;
+    boolean operationsCancelled;
+    File selectedFile;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeTab();
+    }
+
+    abstract void initializeTab();
+
+    @FXML
+    void onBrowseButtonFired(ActionEvent actionEvent) {
+        FileChooser.ExtensionFilter extensionFilter = getFileChooserExtensionFilter();
+        File chooserFile;
+        if (extensionFilter != null) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle(filePathTitleLabel.getText());
+            fileChooser.getExtensionFilters().add(extensionFilter);
+            fileChooser.setInitialDirectory(EmojiTools.getRootDirectory());
+            chooserFile = fileChooser.showOpenDialog(browseButton.getScene().getWindow());
+        } else {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            directoryChooser.setTitle(filePathTitleLabel.getText());
+            directoryChooser.setInitialDirectory(EmojiTools.getRootDirectory());
+            chooserFile = directoryChooser.showDialog(browseButton.getScene().getWindow());
+        }
+
+        if (chooserFile != null) {
+            if (validateSelectedFile(chooserFile)) {
+                this.selectedFile = chooserFile;
+                this.filePathField.setText(selectedFile.getName());
+            }
+        }
+    }
+
+    protected abstract FileChooser.ExtensionFilter getFileChooserExtensionFilter();
+
+    protected abstract boolean validateSelectedFile(File file);
+
+    @FXML
+    void onOpenRootDirectoryButtonFired(ActionEvent actionEvent) {
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.open(EmojiTools.getRootDirectory());
+        } catch (IOException e1) {
+            EmojiTools.submitError(Thread.currentThread(), e1);
+        }
+    }
+
+    @FXML
+    void onStartButtonFired(ActionEvent actionEvent) {
+        startOperations();
+    }
+
+    abstract void startOperations();
+
+    public void stopOperations() {
+        if (this.currentOperationManager != null)
+            this.currentOperationManager.stop();
+        this.operationsCancelled = true;
+    }
+}
---
+++ src/main/java/net/liveforcode/emojitools/renaming2/RenamingThread.java
@@ -0,0 +1,171 @@
+/*
+ * Emoji Tools helps users and developers of Android, iOS, and OS X extract, modify, and repackage Emoji fonts.
+ * Copyright (C) 2015 Mitch Talmadge
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact Mitch Talmadge at mitcht@liveforcode.net
+ */
+
+package net.liveforcode.emojitools.renaming2;
+
+import net.liveforcode.emojitools.oldgui2.RenamingDialog;
+
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class RenamingThread extends Thread {
+
+    private final File renameFile;
+    private final RenamingManager renamingManager;
+    private final RenamingDialog renamingDialog;
+
+    private final boolean[] prefixButtons;
+    private final boolean[] capitalizationButtons;
+
+    private boolean running = true;
+
+
+    private int totalFileNum = 0;
+    private int currentFileNum = 0;
+
+    public RenamingThread(File renameFile, RenamingManager renamingManager, RenamingDialog renamingDialog, boolean[] prefixButtons, boolean[] capitalizationButtons) {
+        super("RenamingThread");
+        this.renameFile = renameFile;
+        this.renamingManager = renamingManager;
+        this.renamingDialog = renamingDialog;
+
+        this.prefixButtons = prefixButtons;
+        this.capitalizationButtons = capitalizationButtons;
+    }
+
+    @Override
+    public void run() {
+
+        File[] files;
+
+        if (renameFile.isDirectory()) {
+            files = renameFile.listFiles();
+        } else {
+            files = new File[]{renameFile};
+        }
+
+        totalFileNum = files.length;
+
+        for (File file : files) {
+            if (file.isDirectory())
+                continue;
+            if (!file.getName().toLowerCase().endsWith(".png"))
+                continue;
+            if (!running) {
+                this.renamingDialog.dispose();
+                return;
+            }
+
+            this.currentFileNum++;
+            String newFileName = file.getName();
+
+            if (prefixButtons != null) {
+                if (prefixButtons[1])
+                    newFileName = changePrefix(file.getName(), "");
+                else if (prefixButtons[2])
+                    newFileName = changePrefix(file.getName(), "uni");
+                else if (prefixButtons[3])
+                    newFileName = changePrefix(file.getName(), "u");
+            }
+
+            if (capitalizationButtons != null) {
+                if (capitalizationButtons[1]) {
+                    newFileName = capitalize((newFileName.equals("")) ? file.getName() : newFileName, (prefixButtons != null && prefixButtons[1]) || !capitalizationButtons[3]);
+                } else if (capitalizationButtons[2]) {
+                    newFileName = newFileName.toLowerCase();
+                }
+            }
+
+            System.out.println("Renaming " + file.getName() + " to " + newFileName);
+            renamingDialog.appendToStatus("Renaming " + file.getName() + " to " + newFileName);
+            updateProgress();
+
+            file.renameTo(new File(file.getParent(), newFileName));
+        }
+
+        updateProgress();
+
+        renamingDialog.dispose();
+    }
+
+    private String changePrefix(String fileName, String newPrefix) {
+        String[] fileNameSplit = fileName.split("\\.");
+        String[] unicodeNames = fileNameSplit[0].split("_");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 0; i < unicodeNames.length; i++)
+        {
+            String name = unicodeNames[i].replaceAll("(?i)(_?)(uni?|u?)([A-Fa-f0-9]+)", "$1"+newPrefix+"$3");
+            if(i > 0)
+                stringBuilder.append("_").append(name);
+            else
+                stringBuilder.append(name);
+        }
+
+        StringBuilder newFileNameBuilder = new StringBuilder(stringBuilder.toString());
+        for(int i = 1; i < fileNameSplit.length; i++)
+        {
+            newFileNameBuilder.append(".").append(fileNameSplit[i]);
+        }
+        return newFileNameBuilder.toString();
+    }
+
+    private String capitalize(String fileName, boolean capitalizePrefix) {
+        String[] fileNameSplit = fileName.split("\\.");
+        String[] unicodeNames = fileNameSplit[0].split("_");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Pattern pattern = Pattern.compile("(?i)(_?)(uni?|u?)([A-Fa-f0-9]+)");
+        for(int i = 0; i < unicodeNames.length; i++)
+        {
+            Matcher matcher = pattern.matcher(unicodeNames[i]);
+            StringBuffer stringBuffer = new StringBuffer();
+            while(matcher.find())
+            {
+                if(capitalizePrefix)
+                    matcher.appendReplacement(stringBuffer, matcher.group(1) + (matcher.group(2) + matcher.group(3)).toUpperCase());
+                else
+                    matcher.appendReplacement(stringBuffer, matcher.group(1) + matcher.group(2) + matcher.group(3).toUpperCase());
+            }
+            matcher.appendTail(stringBuffer);
+
+            if(i > 0)
+                stringBuilder.append("_").append(stringBuffer.toString());
+            else
+                stringBuilder.append(stringBuffer.toString());
+        }
+
+        StringBuilder newFileNameBuilder = new StringBuilder(stringBuilder.toString());
+        for(int i = 1; i < fileNameSplit.length; i++)
+        {
+            newFileNameBuilder.append(".").append(fileNameSplit[i]);
+        }
+        return newFileNameBuilder.toString();
+    }
+
+    private void updateProgress() {
+        renamingDialog.setProgress((int) (((double) currentFileNum / totalFileNum) * 100));
+    }
+
+    public void endRenaming() {
+        running = false;
+    }
+}
