8a5313054ba69e85a9ff8493425d0d2bd0fcb8a0
Mitch Talmadge <mitcht@liveforcode.net>
2016-01-01 21:54:40 -0700

    Beginning work on ProgressDialog

---
+++ src/main/java/net/liveforcode/emojitools/gui/dialogs/ProgressDialog.java
@@ -0,0 +1,17 @@
+package net.liveforcode.emojitools.gui.dialogs;
+
+public class ProgressDialog {
+
+    private final String header;
+
+    public ProgressDialog(String header)
+    {
+        this.header = header;
+    }
+
+    public void display()
+    {
+
+    }
+
+}
